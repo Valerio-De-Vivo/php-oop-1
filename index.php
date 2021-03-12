e85f6a9281c16fa54d27770479fd929aa82c2364
Valerio <valeriodevivo90@gmail.com>
2021-03-12 17:30:00 +0100

    start with methods

--- index.php
+++ index.php
@@ -9,20 +9,28 @@ class  Movie
     public $durata;
     public $serietv;
     public $numero_episodi;
+    public $fonte;
+    public $prezzo;
 
+    function __construct($fonte) {
+        $this->fonte = $fonte;
+    }
 
 }
 
 
-$film1 = new Movie();
+$film1 = new Movie('netflix');
 $film1->titolo = 'The 100';
 $film1->genere = 'fantasy';
 $film1->durata = 130;
 $film1->serietv = true;
 $film1->numero_episodi = 45;
 
+
+
 echo 'Titolo: '.$film1->titolo.'<br>';
 echo 'Genere: '.$film1->genere.'<br>';
 echo 'Durata: '.$film1->durata.'<br>';
 echo 'SerieTv: '.$film1->titolo.'<br>';
 echo 'numero episodi: '.$film1->numero_episodi.'<br>';
+echo 'fonte: '.$film1->fonte.'<br>';
